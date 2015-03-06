6e4f0bfc597f192e883e9eb4f1e61ed02b83885e
Jonas Hietala <mail@jonashietala.se>
2015-03-06 17:03:34 +0100

    Compile with latest rustc.
    $ rustc --version
    rustc 1.0.0-dev (b0746ff19 2015-03-05) (built 2015-03-06)
    
    Without some features however.

--- src/game.rs
+++ src/game.rs
@@ -1,14 +1,13 @@
-
-extern crate shader_version;
 extern crate sdl2_window;
 
 use opengl_graphics::{ Gl, Texture };
 use graphics::*;
-use event::{ Event, RenderArgs, UpdateArgs };
-use event::Event::{ Render, Update, Input };
-use input::*;
-use input::keyboard::Key;
+use piston::event::{ Event, RenderArgs, UpdateArgs };
+use piston::event::Event::{ Render, Update, Input };
+use piston::input::*;
 use sprite::*;
+use shader_version::OpenGL;
+
 use ai_behavior::{
     Action,
     Sequence,
@@ -29,38 +28,41 @@ pub struct Game {
 
 impl Game {
     pub fn new() -> Game {
-        let opengl = shader_version::opengl::OpenGL_3_2;
+        let opengl = OpenGL::_3_2;
 
         let mut scene = Scene::new();
         let tex = Path::new("./bin/assets/unsupported.jpg");
-        let tex = Rc::new(Texture::from_path(&tex).unwrap());
-        let mut sprite = Sprite::from_texture(tex.clone());
+        // TODO next line crashes!! :)
+        //let tex = Rc::new(Texture::from_path(&tex).unwrap());
+
+        //let mut sprite = Sprite::from_texture(tex.clone());
+
         //let image = Texture::from_path(&image).unwrap();
 
-        sprite.set_position(100.0, 100.0);
+        //sprite.set_position(100.0, 100.0);
 
         //let x: int = sprite;
 
-        let id = scene.add_child(sprite);
-        println!("{}", id);
+        //let id = scene.add_child(sprite);
+        //println!("{}", id);
 
-        let seq = Sequence(vec![
-            Action(Ease(EaseCubicOut, box ScaleTo(2.0, 0.5, 0.5))),
-            Action(Ease(EaseBounceOut, box MoveBy(1.0, 0.0, 100.0))),
-            Action(Ease(EaseElasticOut, box MoveBy(2.0, 0.0, -100.0))),
-            Action(Ease(EaseBackInOut, box MoveBy(1.0, 0.0, -100.0))),
+        /*let seq = Sequence(vec![
+            Action(Ease(EaseCubicOut, Box::new(ScaleTo(2.0, 0.5, 0.5)))),
+            Action(Ease(EaseBounceOut, Box::new(MoveBy(1.0, 0.0, 100.0)))),
+            Action(Ease(EaseElasticOut, Box::new(MoveBy(2.0, 0.0, -100.0)))),
+            Action(Ease(EaseBackInOut, Box::new(MoveBy(1.0, 0.0, -100.0)))),
             Wait(0.5),
-            Action(Ease(EaseExponentialInOut, box MoveBy(1.0, 0.0, 100.0))),
+            Action(Ease(EaseExponentialInOut, Box::new(MoveBy(1.0, 0.0, 100.0)))),
             Action(Blink(1.0, 5)),
-            While(box WaitForever, vec![
-                Action(Ease(EaseQuadraticIn, box FadeOut(1.0))),
-                Action(Ease(EaseQuadraticOut, box FadeIn(1.0))),
-                ]),
+            While(Box::new(WaitForever, vec![
+                Action(Ease(EaseQuadraticIn, Box::new(FadeOut(1.0)))),
+                Action(Ease(EaseQuadraticOut, Box::new(FadeIn(1.0)))),
+                ])),
         ]);
-        scene.run(id, &seq);
+        scene.run(id, &seq);*/
 
-        let rotate = Action(Ease(EaseExponentialInOut, box RotateTo(2.0, 360.0)));
-        scene.run(id, &rotate);
+        //let rotate = Action(Ease(EaseExponentialInOut, Box::new(RotateTo(2.0, 360.0))));
+        //scene.run(id, &rotate);
 
 
         Game {
@@ -75,11 +77,11 @@ impl Game {
         self.gl.viewport(0, 0, args.width as i32, args.height as i32);
 
         let c = Context::abs(args.width as f64, args.height as f64);
-        c.color([1.0, 1.0, 1.0, 1.0]).draw(&mut self.gl);
+        //c.color([1.0, 1.0, 1.0, 1.0]).draw(&mut self.gl);
 
         //self.sprite.draw(&c, &mut self.gl);
 
-        self.scene.draw(&c, &mut self.gl);
+        //self.scene.draw(&c, &mut self.gl);
 
         //let x = 20.0;
         //let y = x;
@@ -100,14 +102,14 @@ impl Game {
     }
 
     // http://www.rust-ci.org/PistonDevelopers/piston/doc/piston/input/enum.InputEvent.html
-    pub fn input(&mut self, args: &InputEvent) {
+    pub fn input(&mut self, args: &Input) {
         self.scene.event(args);
 
         //println!("Input: {}", args);
         match args {
             //&Move(MouseCursor(x, y)) => println!("Mouse pos at: {},{}", x, y),
-            &Press(Keyboard(Key::A)) => println!("key 'A' pressed!"),
-            &Press(Keyboard(x)) => println!("Other key {} pressed!", x),
+            &Input::Press(Button::Keyboard(Key::A)) => println!("key 'A' pressed!"),
+            &Input::Press(Button::Keyboard(x)) => println!("Other key {:?} pressed!", x),
             _ => {},
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@
 #![feature(globs)]
 
 extern crate shader_version;
-extern crate input;
 extern crate sprite;
-extern crate event;
 extern crate graphics;
 extern crate sdl2_window;
 extern crate opengl_graphics;
@@ -13,9 +11,11 @@ extern crate piston;
 
 use std::cell::RefCell;
 
-use event::Event::{ Render, Update, Input };
-use event::{ Events, WindowSettings };
+use piston::event::Event::{ Render, Update, Input, Idle };
+use piston::event::{ Events };
+use piston::window::WindowSettings;
 use sdl2_window::Sdl2Window;
+use shader_version::OpenGL;
 
 use game::*;
 
@@ -23,7 +23,7 @@ mod game;
 
 fn main() {
     let window = Sdl2Window::new(
-        shader_version::opengl::OpenGL_3_2,
+        OpenGL::_3_2,
         WindowSettings {
             title: "SmallCity".to_string(),
             size: [600, 400],
@@ -39,6 +39,7 @@ fn main() {
             Render(ref args) => game.render(args),
             Update(ref args) => game.update(args),
             Input(ref args) => game.input(args),
+            Idle(_) => {},
         }
     }
 }
